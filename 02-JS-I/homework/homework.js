00b86d895df83c47d1a7906a44d7763d322f6e1f
geohernandez93 <gemmanuel.her08@gmail.com>
2022-06-01 21:11:31 -0300

    termine la tarea 02-js commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -55,7 +55,8 @@ function multiplica(x, y) {
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  function dividir(x, y)
+  return "valor" ();
 }
 
 function sonIguales(x, y) {
